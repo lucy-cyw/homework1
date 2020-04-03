d7b8ae05780e685cc01f92b554810acbb9239b77
lucy-cyw <15253783673@163.com>
2020-04-03 13:21:50 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180104/Ch4Activity3.java
@@ -0,0 +1,12 @@
+package cn.edu.sdwu.android.classroom.sn170507180104;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ch4Activity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180104/Ch4_2Activity.java
@@ -0,0 +1,12 @@
+package cn.edu.sdwu.android.classroom.sn170507180104;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ch4_2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
